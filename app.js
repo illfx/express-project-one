146764fe5a7b30f0ffeee74b40fb9bddc858280c
Stephen Yoachum <spam@yoachum.net>
2018-03-03 00:39:25 -0500

    Validation on Key/Value Input + Delete Key Capability

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
 
+var index = require('./routes/index');
 var redis = require('./routes/redis');
 
 var app = express();
@@ -19,15 +20,15 @@ nunjucks.configure('views', {
 app.set('view engine', 'njk');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', index);
 app.use('/redis', redis);
-// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/redis.js
+++ routes/redis.js
@@ -1,22 +1,20 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-var redis = require('redis');
+let redis = require('redis');
 
 // create a new redis client and connect to our local redis instance
-var client = redis.createClient();
+let client = redis.createClient();
 
 // if an error occurs, print it to the console
-client.on('error', function (err) {
+client.on('error', (err) => {
     console.log("Error " + err);
 });
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//     res.render('index.njk');
-// });
-
-router.get('', function(req, res) {
+/**
+ * IT IS NOT RECOMMENDED TO USE THIS TYPE OF LOOKUP
+ */
+router.get('/', function(req, res) {
     client.keys('*', function (err, keys) {
 
             if (err) return console.log(err);
@@ -38,14 +36,24 @@ router.get('', function(req, res) {
     });
 });
 
-router.get('/:key', function(req, res) {
-    client.get(req.params.key, function(err, value){
+router.get('/:key', (req, res) => {
+    client.get(req.params.key, (err, value) => {
         res.send(value);
     });
 });
 
-router.post('/redis/store', function(req, res) {
-    client.set(req.body.key, req.body.value, redis.print);
+router.delete('/:key', (req, res) =>{
+    client.del(req.params.key);
+    res.end();
+});
+
+router.post('/store', (req, res) => {
+    if(req.body.key.trim() !== ""){
+        client.set(req.body.key, req.body.value);
+        res.redirect('/redis');
+    }else{
+        res.render('redis/index.njk', { errors: { key : 'A key is required'}, old: req.body });
+    }
 });
 
 module.exports = router;
